11e06c4fbaf6545ee7f89800a26d1277333ec847
michaelsiegrist88 <michaelsiegrist88@a39c44bc-ff8f-11dd-b507-8b4ecd5210be>
2009-02-21 02:19:12 +0000

    Initial upload

---
+++ trunk/sys161-1.14/build-stat161/defs.mk
@@ -0,0 +1,12 @@
+# Automatically generated file; do not edit
+CC=gcc
+CFLAGS= -O3
+LDFLAGS=
+LIBS=
+
+PROG=stat161
+
+INSTALLDIR=/u/msiegris/cs161/bin/
+
+S=..
+
---
+++ trunk/sys161-1.14/build-trace161/defs.mk
@@ -0,0 +1,12 @@
+# Automatically generated file; do not edit
+CC=gcc
+CFLAGS= -O3 -DUSE_TRACE
+LDFLAGS=
+LIBS=
+
+PROG=trace161
+
+INSTALLDIR=/u/msiegris/cs161/bin/
+
+S=..
+
